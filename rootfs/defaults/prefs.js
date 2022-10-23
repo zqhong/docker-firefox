fbbee77fedb3035cc1dc1ab497e8eddc5e92d766
hong <1708674+gt9000@user.noreply.gitee.com>
2022-10-23 22:27:19 +0800

    improve: prefs.js，添加减少内存占用的配置

--- rootfs/defaults/prefs.js
+++ rootfs/defaults/prefs.js
@@ -1,2 +1,13 @@
 // Disable the privacy notice page.
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+
+// Turn off unused features
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+user_pref("identity.fxaccounts.enabled", false);
+
+// Reduce memory usage
+user_pref("browser.sessionhistory.max_total_viewers", 0);
+user_pref("browser.newtab.preload", false);
+user_pref("dom.ipc.processPrelaunch.enabled", false);
+user_pref("browser.cache.memory.enable", false);
