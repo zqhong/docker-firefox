b9ddaf692081f7bd0ade3fc209ebed4cbbb63a7b
hong <1708674+gt9000@user.noreply.gitee.com>
2022-10-25 12:43:25 +0800

    improve: chromium 默认全屏；解决崩溃问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,25 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 # open virtual desktop
+sudo rm -rf /tmp/.X1-lock
+sudo pgrep -a Xvfb && killall Xvfb
+sudo pgrep -a x11vnc && killall x11vnc
+sudo pgrep -a chromium-browser && killall chromium-browser
+
 export DISPLAY=:1
 
 ! pgrep -a Xvfb && Xvfb $DISPLAY -screen 0 "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x24" &
 sleep 3
+# _XSERVTransmkdir: Owner of /tmp/.X11-unix should be set to r
 
-! pgrep -a x11vnc && x11vnc -passwd "$VNC_PASSWORD" -autoport "$VNC_LISTENING_PORT" -bg -forever -nopw -quiet -display WAIT$DISPLAY &
+! pgrep -a x11vnc && x11vnc -geometry "${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}" \
+  -passwd "$VNC_PASSWORD" \
+  -autoport "$VNC_LISTENING_PORT" \
+  -bg -forever -nopw -quiet \
+  -display WAIT$DISPLAY &
 
-chromium-browser --start-fullscreen
+# https://peter.sh/experiments/chromium-command-line-switches/
+chromium-browser --disable-gpu \
+  --disable-dev-shm-usage \
+  --window-size="${DISPLAY_WIDTH},${DISPLAY_HEIGHT}" \
+  --window-position=0,0
